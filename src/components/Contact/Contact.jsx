03954a18ea33a37a7455fc2a70ddfaad54327db8
designs-by-kate <kate_diet@hotmail.com>
2024-03-03 22:23:44 +0000

    added email address

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:katepospiech@hotmail.com">myemail@email.com</a>
+          <a href="mailto:katepospiech@hotmail.com">katepospiech@hotmail.com</a>
         </li>
         <li className={styles.link}>
           <img
