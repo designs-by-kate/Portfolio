b68cfdd2a5289324c862ad8fece2d706592dae88
designs-by-kate <kate_diet@hotmail.com>
2024-03-04 20:24:43 +0000

    Implemented scrolToTop functiona and add logic to redirect to home page if the path is not recognized

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
+
 
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
@@ -9,22 +10,40 @@ import { Experience } from "./components/Experience/Experience";
 import { Projects } from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
 import { Skills } from "./components/Skills/Skills";
-// import { ScrollToTop } from "./components/ScrollToTop";
+// import { NavLink } from 'react-router-dom';
+
+const validRoutes = ['/Portfolio/', '/about', '/skills', '/experience', '/projects', '/contact'];
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to the top of the page when the route changes
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  // Add logic to redirect to home page if the path is not recognized
+  if (!validRoutes.includes(pathname)) {
+    return <Navigate to="/Portfolio/" replace />;
+  }
+
+  return null; // This component doesn't render anything
+};
 
 function App() {
   return (
     <Router>
-      {/* <ScrollToTop /> */}
       <div className={styles.App}>
-      <Navbar />
-      <Routes>
-        <Route path="/Portfolio/" element={<Hero />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/Portfolio" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </div>
     </Router>
   );
