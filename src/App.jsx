70f45feed588cc47b0973ecebfc00447288918cd
designs-by-kate <kate_diet@hotmail.com>
2024-03-04 14:37:13 +0000

    Added to fixed issue : import { HashRouter as Router } from 'react-router-dom';

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
